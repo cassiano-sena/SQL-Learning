0f7d4c93b0e2f3b3fb93f22d0d13520d864f1dab
cassiano-sena <csdscr@gmail.com>
2022-06-01 14:58:34 -0300

    Add files via upload

---
+++ dio.sql
@@ -0,0 +1,10 @@
+CREATE TABLE pessoas (
+    id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(30) NOT NULL,
+    nascimento DATE
+)
+
+INSERT INTO pessoas (nome, nascimento) VALUES ('Cassiano', '2003-03-20')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Cassiano II', '1950-01-10')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Cassius', '1912-02-24')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Cássio', '1995-07-01')
